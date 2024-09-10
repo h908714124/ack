f3e1646e04f2b1c93abc0f0f604f01261cdb16cf
jbock <jbock-java@gmx.de>
2024-09-10 08:10:37 +0200

    use stdint

--- src/ack.c
+++ src/ack.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-void logDebug(long state[], long goal[], long value, int m);
+void logDebug(uint64_t state[], uint64_t goal[], uint64_t value, uint32_t m);
 
-int ackermann(long m, long n) {
-  long state[m], goal[m - 1];
-  for (int i = 0; i < m - 1; i++) {
+uint32_t ackermann(uint64_t m, uint64_t n) {
+  uint64_t state[m], goal[m - 1];
+  for (uint32_t i = 0; i < m - 1; i++) {
     state[i] = 0;
     goal[i] = 1;
   }
   state[m - 1] = 0;
-  long value = 1;
+  uint64_t value = 1;
   logDebug(state, goal, value, m);
   while (state[m - 1] != n + 1) {
     value++;
-    for (int i = 0; i < m; i++) {
+    for (uint32_t i = 0; i < m; i++) {
       if (state[i]++ != goal[i]) {
         break;
       }
@@ -27,35 +28,35 @@ int ackermann(long m, long n) {
   return value;
 }
 
-void logDebug(long *state, long *goal, long value, int m) {
+void logDebug(uint64_t *state, uint64_t *goal, uint64_t value, uint32_t m) {
   #ifdef DEBUG
-  printf("%ld   state: ", value);
-  for (int i = 0; i < m; i++) {
-    printf("%ld ", state[i]);
+  printf("%d   state: ", value);
+  for (uint32_t i = 0; i < m; i++) {
+    printf("%d ", state[i]);
   }
   printf("  goal: ");
-  for (int i = 0; i < m - 1; i++) {
-    printf("%ld ", goal[i]);
+  for (uint32_t i = 0; i < m - 1; i++) {
+    printf("%d ", goal[i]);
   }
   printf("\n");
   #endif
 }
 
-int main(int argc, char **argv) {
+uint32_t main(uint32_t argc, char **argv) {
   char* end;
   if (argc != 3) {
     printf("Expecting 2 arguments but found %d\n", argc - 1);
     return 1;
   }
-  long m = strtol(argv[1], &end, 10);
-  long n = strtol(argv[2], &end, 10);
+  uint64_t m = strtol(argv[1], &end, 10);
+  uint64_t n = strtol(argv[2], &end, 10);
   if (m >= 4 && n >= 2) {
-    printf("ack(%ld, %ld) may take a _very_ long time to compute.\n", m, n);
+    printf("ack(%d, %d) may take a _very_ long time to compute.\n", m, n);
     printf("Try with smaller values, or press Return to continue anyway.\n");
     char buf[10];
     fgets(buf, 10, stdin);
   }
-  long result = ackermann(m, n);
-  printf("ackermann(%ld, %ld) = %ld\n", m, n, result);
+  uint64_t result = ackermann(m, n);
+  printf("ackermann(%d, %d) = %d\n", m, n, result);
   return 0;
 }
