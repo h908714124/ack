62bd3db383788312f2e4bb037ff515261599834a
jbock <jbock-java@gmx.de>
2024-09-10 08:26:13 +0200

    use some ints

--- src/ack.c
+++ src/ack.c
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void logDebug(uint64_t state[], uint64_t goal[], uint64_t value, uint32_t m);
+void logDebug(uint64_t counters[], uint64_t goals[], uint64_t value, int m);
 
-uint32_t ackermann(uint64_t m, uint64_t n) {
-  uint64_t state[m], goal[m - 1];
-  for (uint32_t i = 0; i < m - 1; i++) {
-    state[i] = 0;
-    goal[i] = 1;
+int ackermann(uint64_t m, uint64_t n) {
+  uint64_t counters[m], goals[m - 1];
+  for (int i = 0; i < m - 1; i++) {
+    counters[i] = 0;
+    goals[i] = 1;
   }
-  state[m - 1] = 0;
+  counters[m - 1] = 0;
   uint64_t value = 1;
-  logDebug(state, goal, value, m);
-  while (state[m - 1] != n + 1) {
+  logDebug(counters, goals, value, m);
+  while (counters[m - 1] != n + 1) {
     value++;
-    for (uint32_t i = 0; i < m; i++) {
-      if (state[i]++ != goal[i]) {
+    for (int i = 0; i < m; i++) {
+      if (counters[i]++ != goals[i]) {
         break;
       }
       if (i != m - 1) {
-        goal[i] = value;
+        goals[i] = value;
       }
     }
-    logDebug(state, goal, value, m);
+    logDebug(counters, goals, value, m);
   }
   return value;
 }
 
-void logDebug(uint64_t *state, uint64_t *goal, uint64_t value, uint32_t m) {
+void logDebug(uint64_t *counters, uint64_t *goals, uint64_t value, int m) {
   #ifdef DEBUG
-  printf("%d   state: ", value);
-  for (uint32_t i = 0; i < m; i++) {
-    printf("%d ", state[i]);
+  printf("%d   counters: ", value);
+  for (int i = 0; i < m; i++) {
+    printf("%d ", counters[i]);
   }
-  printf("  goal: ");
-  for (uint32_t i = 0; i < m - 1; i++) {
-    printf("%d ", goal[i]);
+  printf("  goals: ");
+  for (int i = 0; i < m - 1; i++) {
+    printf("%d ", goals[i]);
   }
   printf("\n");
   #endif
 }
 
-uint32_t main(uint32_t argc, char **argv) {
+int main(int argc, char **argv) {
   char* end;
   if (argc != 3) {
     printf("Expecting 2 arguments but found %d\n", argc - 1);
@@ -50,12 +50,6 @@ uint32_t main(uint32_t argc, char **argv) {
   }
   uint64_t m = strtol(argv[1], &end, 10);
   uint64_t n = strtol(argv[2], &end, 10);
-  if (m >= 4 && n >= 2) {
-    printf("ack(%d, %d) may take a _very_ long time to compute.\n", m, n);
-    printf("Try with smaller values, or press Return to continue anyway.\n");
-    char buf[10];
-    fgets(buf, 10, stdin);
-  }
   uint64_t result = ackermann(m, n);
   printf("ackermann(%d, %d) = %d\n", m, n, result);
   return 0;
