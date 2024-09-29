5c55e959821f60dd48a0328c1a2768015aaf2453
jbock <jbock-java@gmx.de>
2024-09-29 20:23:46 +0200

    up down

--- src/ack.c
+++ src/ack.c
@@ -3,11 +3,14 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <curses.h>
+#include <string.h>
 
-void printRow(uint64_t counters[], uint64_t goals[], uint64_t value);
+static const int MAX_ACK = 6;
+
+void printScreen(uint64_t counters[], uint64_t goals[], uint64_t value);
 
 void increment(uint64_t *counters, uint64_t *goals, uint64_t value) {
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < MAX_ACK; i++) {
     if (counters[i]++ != goals[i]) {
       break;
     }
@@ -16,7 +19,7 @@ void increment(uint64_t *counters, uint64_t *goals, uint64_t value) {
 }
 
 void decrement(uint64_t *counters, uint64_t *goals, uint64_t value) {
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < MAX_ACK; i++) {
     counters[i]--;
     if (goals[i] != value) {
       break;
@@ -28,24 +31,39 @@ void decrement(uint64_t *counters, uint64_t *goals, uint64_t value) {
 int main() {
   char c;
   initscr();
-  clear();
-  refresh();
-  uint64_t counters[6], goals[6];
-  for (int i = 0; i < 6; i++) {
+  curs_set(0);
+  uint64_t counters[MAX_ACK], goals[MAX_ACK];
+  for (int i = 0; i < MAX_ACK; i++) {
     counters[i] = 0;
     goals[i] = 1;
   }
   uint64_t value = 1;
-  printRow(counters, goals, value);
+  printScreen(counters, goals, value);
   while (1) {
-    printRow(counters, goals, value);
     scanf("%c", &c);
     if (c == 'j') {
       value++;
       increment(counters, goals, value);
+      printScreen(counters, goals, value);
     } else if (c == 'k' && value != 1) {
       decrement(counters, goals, value);
       value--;
+      printScreen(counters, goals, value);
+    } else if (c == 'u') {
+      for (int i = 0; i < 20; i++) {
+        if (value == 1) {
+          break;
+        }
+        decrement(counters, goals, value);
+        value--;
+      }
+      printScreen(counters, goals, value);
+    } else if (c == 'd') {
+      for (int i = 0; i < 20; i++) {
+        value++;
+        increment(counters, goals, value);
+      }
+      printScreen(counters, goals, value);
     } else if (c == 'q') {
       break;
     }
@@ -55,15 +73,30 @@ int main() {
 }
 
 void printRow(uint64_t *counters, uint64_t *goals, uint64_t value) {
-  clear();
   printw("%llu   counters: ", value);
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < MAX_ACK; i++) {
     printw("%llu ", counters[i]);
   }
   printw("  goals: ");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < MAX_ACK; i++) {
     printw("%llu ", goals[i]);
   }
   printw("\n");
+}
+
+void printScreen(uint64_t *counters, uint64_t *goals, uint64_t value) {
+  uint64_t goalsCopy[MAX_ACK];
+  uint64_t countersCopy[MAX_ACK];
+  memcpy(goalsCopy, goals, MAX_ACK * sizeof(uint64_t));
+  memcpy(countersCopy, counters, MAX_ACK * sizeof(uint64_t));
+  clear();
+  printRow(countersCopy, goalsCopy, value);
+  int col, row;
+  getmaxyx(stdscr, row, col);
+  for (int i = 1; i < row; i++) {
+    value++;
+    increment(countersCopy, goalsCopy, value);
+    printRow(countersCopy, goalsCopy, value);
+  }
   refresh();
 }
